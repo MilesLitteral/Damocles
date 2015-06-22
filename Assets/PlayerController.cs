e28b742ef1c80324a8dab41abb88095bdafa7327
SasoriZero <Mandaloe2@gmail.com>
2015-06-22 02:26:01 -0400

    Damocles init
    Initialized and uploaded

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dbound 
+{
+	public float xMin, xMax, yMin, yMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody rb;
+	public Dbound Boundary;
+	public float tilt;
+
+	//private float nextFire;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+	}
+
+	void onCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Enemy") 
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
